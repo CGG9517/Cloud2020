0bbe1fd01f5dc955122079a0efadaee609fa19d1
Jiang Chao <840065231@qq.com>
2020-12-27 23:41:56 +0800

    #comment spring cloud eureka服务注册中心集群配置

--- cloud-eureka-server7002/src/main/java/com/jiangchao/springcloud/EurekaServerApplication.java
+++ cloud-eureka-server7002/src/main/java/com/jiangchao/springcloud/EurekaServerApplication.java
@@ -1,14 +1,14 @@
 package com.jiangchao.springcloud;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentApplication {
+@EnableEurekaServer
+public class EurekaServerApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(PaymentApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
